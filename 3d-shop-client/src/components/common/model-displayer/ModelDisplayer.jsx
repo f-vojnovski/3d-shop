073865d571177588ec566fe2fff5473c69be5e70
Filip Vojnovski <c00l4l1f3@gmail.com>
2022-05-12 11:16:55 +0200

    added loader as placeholder

--- 3d-shop-client/src/components/common/model-displayer/ModelDisplayer.jsx
+++ 3d-shop-client/src/components/common/model-displayer/ModelDisplayer.jsx
@@ -1,15 +1,51 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import * as THREE from 'three'
 
 const ModelDisplayer = () => {
+  let fileUrl =
+    "http://127.0.0.1:8000/storage/obj_files/627ca2ce77c65.obj";
+
+  const loader = new THREE.FileLoader();
+
+  loader.load(
+    // resource URL
+    fileUrl,
+
+    // onLoad callback
+    function (data) {
+      // output the text to the console
+      console.log(data);
+    },
+
+    // onProgress callback
+    function (xhr) {
+      console.log(
+        (xhr.loaded / xhr.total) * 100 + "% loaded"
+      );
+    },
+
+    // onError callback
+    function (err) {
+      console.error("An error happened");
+    }
+  );
+
   return (
     <Canvas>
       <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[3, 0, 3]} intensity={0.2}/>
-      <directionalLight color="blue" position={[-3, -3, -3]} />
+      <directionalLight
+        color="red"
+        position={[3, 0, 3]}
+        intensity={0.2}
+      />
+      <directionalLight
+        color="blue"
+        position={[-3, -3, -3]}
+      />
       <mesh>
         <boxGeometry />
-        <meshStandardMaterial color="hotpink"/>
+        <meshStandardMaterial color="hotpink" />
       </mesh>
       <OrbitControls></OrbitControls>
     </Canvas>
