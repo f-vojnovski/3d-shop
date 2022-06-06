e0b4ea7c2f20075050130243ca05054613d5f795
Filip Vojnovski <c00l4l1f3@gmail.com>
2022-06-06 06:27:38 +0200

    Spinner inline size

--- 3d-shop-client/src/components/common/spinner/LoadingSpinner.jsx
+++ 3d-shop-client/src/components/common/spinner/LoadingSpinner.jsx
@@ -1,7 +1,13 @@
 const LoadingSpinner = () => {
   return (
-    <div className="spinner-border" role="status">
-      <span className="sr-only"></span>
+    <div className="centered-item">
+      <div
+        className="spinner-border spinner-dimensions"
+        role="status"
+        style={{ width: '4rem', height: '4rem' }}
+      >
+        <span className="sr-only"></span>
+      </div>
     </div>
   );
 };
