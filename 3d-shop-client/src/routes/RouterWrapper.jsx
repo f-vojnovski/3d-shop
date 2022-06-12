7a2b373c3c5f7a2f83be5352eae02576c9c804cd
Filip Vojnovski <c00l4l1f3@gmail.com>
2022-06-12 04:39:50 +0200

    Rename folders

--- 3d-shop-client/src/routes/RouterWrapper.jsx
+++ 3d-shop-client/src/routes/RouterWrapper.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
-import CheckoutPage from '../components/pages/checkout-page/CheckoutPage';
-import LoginPage from '../components/pages/login-page/LoginPage';
-import SingleProductView from '../components/pages/model-page/SingleProductView';
-import ModelsListPage from '../components/pages/models-list-page/ModelsListPage';
-import RegisterPage from '../components/pages/register-page/RegisterPage';
+import CheckoutPage from '../components/pages/checkout/CheckoutPage';
+import LoginPage from '../components/pages/login/LoginPage';
+import SingleProductView from '../components/pages/product-view/SingleProductView';
+import ModelsListPage from '../components/pages/products-list/ModelsListPage';
+import RegisterPage from '../components/pages/register/RegisterPage';
 
 const RoutesWrapper = () =>
   useRoutes([
