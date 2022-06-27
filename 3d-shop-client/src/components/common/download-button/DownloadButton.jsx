202d336c13411ae7a17811d8abb07bd6e26840d0
Filip Vojnovski <c00l4l1f3@gmail.com>
2022-06-27 10:38:55 +0200

    Fix download button

--- 3d-shop-client/src/components/common/download-button/DownloadButton.jsx
+++ 3d-shop-client/src/components/common/download-button/DownloadButton.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 
 const DownloadButton = (props) => {
+  console.log(props);
+
   return (
-    <Link to={props.link} target="_blank" download>
+    <a href={props.link} target="_blank" download>
       {props.text}
-    </Link>
+    </a>
   );
 };
 export default DownloadButton;
